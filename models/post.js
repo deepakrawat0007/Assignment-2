17687374f90e9f2344928095a1bd3016652a4e98
deepakrawat0007 <deepakrawat3738@gmail.com>
2022-12-10 15:56:05 +0530

    Schema added to git

---
+++ models/post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+    title:{type:String , required : true},
+    body:{type:String , required : true},
+    image:{type : URL , required : true},
+    user:{type:String , required : true}
+},{collection : "Posts" })
+
+const Post = mongoose.model('Post' , PostSchema)
+
+module.exports = Post;
